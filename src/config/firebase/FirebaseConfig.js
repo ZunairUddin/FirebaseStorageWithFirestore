f8180a1c607ec3fcd7515cae9f9e41094e86afb2
Zunair <zunairuddin30@gmail.com>
2023-09-30 17:46:52 +0500

    Update FirebaseConfig.js

--- src/config/firebase/FirebaseConfig.js
+++ src/config/firebase/FirebaseConfig.js
@@ -6,14 +6,6 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   //PASTE UR FIREBASE CONFIG HERE.........
-  apiKey: "AIzaSyAXoU1Y7daHm4X9GKJ4WzKeEkZncw4cWAg",
-  authDomain: "testing-2e13b.firebaseapp.com",
-  databaseURL: "https://testing-2e13b-default-rtdb.firebaseio.com",
-  projectId: "testing-2e13b",
-  storageBucket: "testing-2e13b.appspot.com",
-  messagingSenderId: "503385084408",
-  appId: "1:503385084408:web:e7a19fbf196530b924460d",
-  measurementId: "G-1DF3PWD8HZ",
 };
 
 // Initialize Firebase
